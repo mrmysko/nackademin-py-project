f9bcd51b278a7d03485cf82aaee1b79a32ca8a9b
Kent Larsson <kent@c8.se>
2024-03-12 13:45:30 +0100

    Improved uppgift.py template

--- uppgift.py
+++ uppgift.py
@@ -1,9 +1,11 @@
-# Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
-# viktigt att du ger funktionen exakt det namn som står i beskrivningen.
+# Skriv endast definitioner här på denna indenteringsnivå! Det är viktigt att du
+# namnger funktioner, klasser och variabler exakt med de namn som står i
+# beskrivningen.
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
-    # filen direkt, och inte när du importerar den som modul i en annan fil.
+    # Här kan du skriva testkod som bara körs när du kör filen direkt och inte
+    # när den importeras som modul i en annan fil.
+    #
     # Koden importeras som en modul av autograding-funktionen för att utföra ett
     # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
     # utanför denna if-sats.
@@ -12,4 +14,8 @@ if __name__ == "__main__":
     #
     # print(funktionsnamn("hejsan", 99))
     # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+    #
+    # Exempel:
+    # minklass = Klass()
+    # print(klass.leet("hejsan")
+    pass  # Ta bort denna rad när du skriver din kod
