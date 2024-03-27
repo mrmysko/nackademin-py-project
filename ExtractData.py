f74dbb4c2ec33997947e53d8c8ad1d9df4d7ea61
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-03-27 07:58:34 +0100

    re format name and price

--- ExtractData.py
+++ ExtractData.py
@@ -21,7 +21,16 @@ def ExtractData(url: str) -> tuple:
     # Extracts the name of the product. - Should probably format this to remove Art.nr:
     name = soup.find("div", {"class": "subTitle big"}).get_text().strip()
 
+    # Remove art nr from name.
+    name = re.sub(" Art.nr: \d*$", "", name)
+
     # Selects the div with classname "price-big", gets the text inside the div and strips it of whitespace. Prints as e.g. "2 990:-" - Should prolly remove ":-" and convert this to a int.
     price = soup.find("div", {"class": "price-big"}).get_text().strip()
+
+    # Removes ":-" from price and converts to an int.
+    # Although, when I display it later I want ":-", but it's easier to work with an int for comparison etc and different stores might display price differently.
+    # Has to be a cleaner way to do this.
+    price = re.sub(":-$", "", price)
+    price = "".join(price.split())
 
     return (name, price)
