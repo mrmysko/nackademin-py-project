b9a1e5e6c6da32b87a3c6a57a875b7f9ba725974
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-03-27 07:57:42 +0100

    add remove and print methods

--- DBModule.py
+++ DBModule.py
@@ -1,6 +1,8 @@
 import sqlite3
 
-# Kan en db vara ett klassobjekt?
+# Att man använder placeholders (?) istället för string format har tydligen med att preventa SQL-injections att göra.
+
+# Use with statements to close db-connection and cursor when done.
 
 
 class Database:
@@ -30,6 +32,26 @@ class Database:
         )
 
         self.con.commit()
+
+    def remove_product_data(self, id):
+        # Add confirmation. - BUT, I dont even know if I want the print and confirmation here...thats user facing stuff, only place database methods here.
+        # Do something like, Query for id -> Get that data and display it -> Ask for confirmation -> remove_product_data(id)
+        self.cur.execute("DELETE FROM products WHERE id = ?", id)
+        self.con.commit()
+        print(self.cur.rowcount, "record(s) deleted")
+        input()
+
+    def print_db(self):
+
+        longest_name = 0
+        for id_, name, price_, url_ in self.dump_db():
+            if len(name) > longest_name:
+                longest_name = len(name)
+
+        for id, name, price, url in self.dump_db():
+            print(
+                f"{str(id).rjust(3)} {name.rjust(longest_name)} {price.rjust(0)} {url.rjust(0)}"
+            )
 
     def dump_db(self):
         return self.cur.execute("SELECT * FROM products").fetchall()
